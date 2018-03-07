277c4b9ee79b3e17ab208490e0fcb799affa706f
wuhgogo <wuhgogo@gmail.com>
2018-03-07 23:57:47 +0800

    collection相关类的测试方法

---
+++ src/main/java/com/study/test/collection/CollectionTest.java
@@ -0,0 +1,14 @@
+package com.study.test.collection;
+
+import java.util.LinkedList;
+
+import org.junit.Test;
+
+public class CollectionTest {
+
+	@Test
+	public void addTest() {
+		new LinkedList<String>().add("1");
+	}
+	
+}
